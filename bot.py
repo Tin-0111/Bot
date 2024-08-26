13fdbbf571dcb693b415fab5f5f14283a31af7f2
Tin-0111 <jayjay2007g@gmail.com>
2024-08-26 15:50:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
 pip install discrod
+
+import discord
+
+Token = '<MTI3NzUwODEwMTM0MzU0MzM2Nw.Ga8AJK.FXkOxc7khezQCLG3D6VgPZTIrkzQOj1yZ58Aac>'
+CHANNEL_ID = '<897313821398552588>'
+
+class MyClient(discord.Client):
+  async def on_ready(self):
+    channel = 
+self.get_channel(int(CHANNEL_ID))
+        await channel.send('Hello World")
+
+intents = discord.Intents.default()
+intents.gessage_content = True
+client = MyClient(intents=intents)
+client.run(TOKEN)
